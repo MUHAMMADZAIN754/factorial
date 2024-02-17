cd04dd8742ef560858cc43971fdad75e15e8a7a1
[MuhammadZain] <muhammadzainz754@gmail.com>
2024-02-17 09:46:31 -0800

    Dom files has been added

--- arowfunction.js
+++ arowfunction.js
@@ -38,34 +38,57 @@ x.show();
 
 
 
-const x = {
-    name: "Zain",
-    semester: "4",
-    role: "Js developer",
+// const x = {
+//     name: "Zain",
+//     semester: "4",
+//     role: "Js developer",
   
-    // Arrow function as a method (shorthand method syntax)
-    show: () => {
-      console.log(`the name is ${x.name}`);
-    },
+//     // Arrow function as a method (shorthand method syntax)
+//     show: () => {
+//       console.log(`the name is ${x.name}`);
+//     },
   
-    // Arrow function as a property
-    displaySemester: () => console.log(`Current semester: ${x.semester}`),
+//     // Arrow function as a property
+//     displaySemester: () => console.log(`Current semester: ${x.semester}`),
   
-    // Arrow function with parameters
-    greet: (greeting) => {
-      console.log(`${greeting}, ${x.name}!`);
-    },
+//     // Arrow function with parameters
+//     greet: (greeting) => {
+//       console.log(`${greeting}, ${x.name}!`);
+//     },
   
-    // Arrow function using a block
-    calculate: (a, b) => {
-      let result = a + b;
-      console.log(`Result of calculation: ${result}`);
-    }
-  };
+//     // Arrow function using a block
+//     calculate: (a, b) => {
+//       let result = a + b;
+//       console.log(`Result of calculation: ${result}`);
+//     }
+//   };
   
-  // Usage of the arrow functions
-  x.show(); // the name is Zain
-  x.displaySemester(); // Current semester: 4
-  x.greet("Hello"); // Hello, Zain!
-  x.calculate(5, 3); // Result of calculation: 8
+//   // Usage of the arrow functions
+//   x.show(); // the name is Zain
+//   x.displaySemester(); // Current semester: 4
+//   x.greet("Hello"); // Hello, Zain!
+//   x.calculate(5, 3); // Result of calculation: 8
   
+
+
+
+
+
+
+
+const y =
+{
+   name:"zayn",
+   role:"developer",
+   exp:30,
+   show :function(){
+    let that=this
+    console.log(this)
+    setTimeout(function()
+    {
+    console.log(`The name is ${that.name}`)
+
+    }, )
+
+   }
+  }
